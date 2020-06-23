72b68fcb3dea30f12e0cde607da96159a7855278
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-06-23 02:46:10 -0500

    cors

--- server.js
+++ server.js
@@ -1,8 +1,20 @@
 const express = require('express');
-const server = express();
+const cors = require("cors");
+const helmet = require("helmet");
 const Log = require('./database/model/log-model');
+const server = express();
+server.use(cors());
+server.use(helmet());
 server.use(express.json());
 
+
+server.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+  });
 
 server.get('/', (req, res) => {
     Log.get()
